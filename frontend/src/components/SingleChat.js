f4476162d88a1e79fc8d3e25610de9e25f252e57
najumudheen-systech <129059180+najumudheen-systech@users.noreply.github.com>
2023-05-17 19:32:42 +0530

    before implementing socket

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -1,13 +1,101 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ChatState } from '../Context/ChatProvider'
-import { Box, IconButton, Text } from '@chakra-ui/react'
+import { Box, FormControl, IconButton, Input, Spinner, Text, useToast } from '@chakra-ui/react'
 import { ArrowBackIcon } from '@chakra-ui/icons'
 import { getSender, getSenderFull } from '../config/ChatLogics'
 import ProfileModal from './miscellaneous/ProfileModal'
 import UpdateGroupChatModal from './miscellaneous/UpdateGroupChatModal'
+import axios from 'axios'
+import './styles.css'
+import ScrollableChat from './ScrollableChat'
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
+    const [messages, setMessages] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [newMessage, setNewMessage] = useState("");
+    const [socketConnected, setSocketConnected] = useState(false);
+    const [typing, setTyping] = useState(false);
+    const [istyping, setIsTyping] = useState(false);
+    const toast = useToast();
     const { user, selectedChat, setSelectedChat } = ChatState()
+
+    const fetchMessages = async () => {
+        if (!selectedChat) return;
+    
+        try {
+          const config = {
+            headers: {
+              Authorization: `Bearer ${user.token}`,
+            },
+          };
+    
+          setLoading(true);
+    
+          const { data } = await axios.get(
+            `/api/message/${selectedChat._id}`,
+            config
+          );
+          setMessages(data);
+          setLoading(false);
+    
+        //   socket.emit("join chat", selectedChat._id);
+        } catch (error) {
+          toast({
+            title: "Error Occured!",
+            description: "Failed to Load the Messages",
+            status: "error",
+            duration: 5000,
+            isClosable: true,
+            position: "bottom",
+          });
+        }
+      };
+
+    const sendMessage = async (event) => {
+        if (event.key === "Enter" && newMessage) {
+            // socket.emit("stop typing", selectedChat._id);
+            try {
+                const config = {
+                    headers: {
+                        "Content-type": "application/json",
+                        Authorization: `Bearer ${user.token}`,
+                    },
+                };
+                setNewMessage("");
+                const { data } = await axios.post(
+                    "/api/message",
+                    {
+                        content: newMessage,
+                        chatId: selectedChat._id,
+                    },
+                    config
+                );
+                // socket.emit("new message", data);
+                setMessages([...messages, data]);
+            } catch (error) {
+                toast({
+                    title: "Error Occured!",
+                    description: "Failed to send the Message",
+                    status: "error",
+                    duration: 5000,
+                    isClosable: true,
+                    position: "bottom",
+                });
+            }
+        }
+    }
+
+    useEffect(() => {
+        fetchMessages();
+    
+        // selectedChatCompare = selectedChat;
+        // eslint-disable-next-line
+      }, [selectedChat]);
+
+    const typingHandler = async (e) => {
+        setNewMessage(e.target.value);
+    }
+
     return (
         <>
             {selectedChat ? (
@@ -38,10 +126,10 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
                             <>
                                 {selectedChat.chatName.toUpperCase()}
                                 <UpdateGroupChatModal
-                                // fetchMessages={fetchMessages}
-                                fetchAgain={fetchAgain}
-                                setFetchAgain={setFetchAgain}
-                            />
+                                    fetchMessages={fetchMessages}
+                                    fetchAgain={fetchAgain}
+                                    setFetchAgain={setFetchAgain}
+                                />
                             </>
                         )}
                     </Text>
@@ -57,7 +145,34 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
                         overflowY="hidden"
                     >
                         {/* Messages Here */}
-                        
+                        {loading ? (
+                            <Spinner
+                                size="xl"
+                                w={20}
+                                h={20}
+                                alignSelf="center"
+                                margin="auto"
+                            />
+                        ) : (
+                            <div className="messages">
+                                {/* Messages */}
+                                <ScrollableChat messages={messages} />
+                            </div>
+                        )}
+                        <FormControl
+                            onKeyDown={sendMessage}
+                            id="first-name"
+                            isRequired
+                            mt={3}
+                        >
+                            <Input
+                                variant="filled"
+                                bg="#E0E0E0"
+                                placeholder="Enter a message.."
+                                value={newMessage}
+                                onChange={typingHandler}
+                            />
+                        </FormControl>
                     </Box>
                 </>
             ) : (
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import ChatProvider from './Context/ChatProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChatProvider>
-    <BrowserRouter>
-    <ChakraProvider>
-    <App />
-    </ChakraProvider>
-    </BrowserRouter>
-  </ChatProvider>
+  <BrowserRouter>
+    <ChatProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ChatProvider>
+  </BrowserRouter>
 );
 
 
